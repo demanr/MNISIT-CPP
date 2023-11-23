e194aff5d7a40457371ca886c2dafd7ceaa64f16
Rachelle DeMan <demanr@mcmaster.ca>
2023-11-23 17:44:14 -0500

    Removed unused code

--- engine2/src/mnistData.cpp
+++ engine2/src/mnistData.cpp
@@ -4,43 +4,6 @@
 
 using namespace std;
 
-void printAsciiImage(const std::vector<double> &values)
-{
-    // Check if the size of the input vector is correct
-    cout << "values.size(): " << values.size() << endl;
-
-    if (values.size() != 784)
-    {
-        std::cerr << "Error: Input vector size is not 784." << std::endl;
-        return;
-    }
-
-    // Define characters to represent different intensity levels
-    const char intensityChars[] = {' ', '.', ',', ':', 'o', 'O', 'X', '#', '$', '@'};
-
-    // Calculate the range for each intensity level
-    const double range = 1.0 / (sizeof(intensityChars) / sizeof(intensityChars[0]) - 1);
-
-    // Iterate over the vector and print ASCII characters based on the values
-    for (int i = 0; i < values.size(); ++i)
-    {
-        // Adjust the intensity to a character in the range of ASCII characters
-        int intensityLevel = static_cast<int>(values[i] / range);
-        intensityLevel = std::min(std::max(intensityLevel, 0), static_cast<int>(sizeof(intensityChars) - 1));
-
-        char pixel = intensityChars[intensityLevel];
-
-        // Print the ASCII character
-        std::cout << pixel;
-
-        // Insert a newline character after every 28 characters to create a 28x28 image
-        if ((i + 1) % 28 == 0)
-        {
-            std::cout << std::endl;
-        }
-    }
-}
-
 void loadData(vector<vector<double>> &mnist_train_images, vector<int> &mnist_train_labels, string filepath)
 {
     // Open mnist_train.csv
@@ -85,10 +48,4 @@ void loadData(vector<vector<double>> &mnist_train_images, vector<int> &mnist_tra
     }
     cout << endl
          << endl;
-    /*
-        // print first image
-        for (int i = 0; i < 28; i++)
-        {
-            printAsciiImage(mnist_train_images[i]);
-        }*/
 }
